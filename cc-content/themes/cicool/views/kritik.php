4a7ed8f3441591e1cba00b765080d78a1866ff02
maria_meiliani <maria19si@mahasiswa.pcr.ac.id>
2023-05-16 10:14:23 +0700

    feat: Tambah Animasi Beruang di Kritik

--- cc-content/themes/cicool/views/kritik.php
+++ cc-content/themes/cicool/views/kritik.php
@@ -4,9 +4,81 @@
     <?= get_navigation(); ?>
 
     <body>
+        <style>
+        .loader {
+            width: 160px;
+            height: 185px;
+            position: relative;
+            background: #ADD8E6;
+            border-radius: 100px 100px 0 0;
+            margin-left: 720px;
+        }
 
-        <h3 class="text-center pt-50 " style="color:green">Kritik dan Saran</h3>
+        .loader:after {
+            content: "";
+            position: absolute;
+            width: 100px;
+            height: 125px;
+            left: 50%;
+            top: 25px;
+            transform: translateX(-50%);
+            background-image: radial-gradient(circle, #000 48%, transparent 55%),
+                radial-gradient(circle, #000 48%, transparent 55%),
+                radial-gradient(circle, #fff 30%, transparent 45%),
+                radial-gradient(circle, #000 48%, transparent 51%),
+                linear-gradient(#000 20px, transparent 0),
+                linear-gradient(#cfecf9 60px, transparent 0),
+                radial-gradient(circle, #cfecf9 50%, transparent 51%),
+                radial-gradient(circle, #cfecf9 50%, transparent 51%);
+            background-repeat: no-repeat;
+            background-size: 16px 16px, 16px 16px, 10px 10px, 42px 42px, 12px 3px,
+                50px 25px, 70px 70px, 70px 70px;
+            background-position: 25px 10px, 55px 10px, 36px 44px, 50% 30px, 50% 85px,
+                50% 50px, 50% 22px, 50% 45px;
+            animation: faceLift 3s linear infinite alternate;
+        }
+
+        .loader:before {
+            content: "";
+            position: absolute;
+            width: 140%;
+            height: 125px;
+            left: -20%;
+            top: 0;
+            background-image: radial-gradient(circle, #ADD8E6 48%, transparent 50%),
+                radial-gradient(circle, #ADD8E6 48%, transparent 50%);
+            background-repeat: no-repeat;
+            background-size: 65px 65px;
+            background-position: 0px 12px, 145px 12px;
+            animation: earLift 3s linear infinite alternate;
+        }
+
+        @keyframes faceLift {
+            0% {
+                transform: translateX(-60%);
+            }
+
+            100% {
+                transform: translateX(-30%);
+            }
+        }
+
+        @keyframes earLift {
+            0% {
+                transform: translateX(10px);
+            }
+
+            100% {
+                transform: translateX(0px);
+            }
+        }
+        </style>
+
+
+        <h3 class="text-center pt-50 " style="color:green;">Kritik dan Saran</h3>
         <hr class="hrcenter">
+
+        <div class="loader"></div>
 
         <div id="snackbar">Pesan Terkirim</div>
 
@@ -14,11 +86,11 @@
         <div class="row p-5">
             <div class="col-lg-12">
                 <strong>Nama:</strong>
-                <input type="text" name="nama" id="nama" class="form-control" placeholder="nama"><br>
+                <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama"><br>
             </div>
             <div class="col-lg-12">
                 <strong>Kritik:</strong>
-                <textarea name="kritik" id="kritik" class="form-control" placeholder="kritik"></textarea>
+                <textarea name="kritik" id="kritik" class="form-control" placeholder="Kritik/Pendapat/Saran"></textarea>
             </div>
             <div class="col-lg-12">
                 <br />
