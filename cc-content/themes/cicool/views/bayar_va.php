fcf5f125371557cdc9faf1d5442ec02a6c0521c2
maria_meiliani <maria19si@mahasiswa.pcr.ac.id>
2023-06-03 18:39:07 +0700

    feat: Perbaiki Tampilan Pembayaran VA dan DD

--- cc-content/themes/cicool/views/bayar_va.php
+++ cc-content/themes/cicool/views/bayar_va.php
@@ -3,6 +3,7 @@
 <style>
 .id-card-wrapper .center {
     justify-content: center;
+    align-items: center;
     height: 100vh;
     width: 100%;
     display: flex;
@@ -21,6 +22,7 @@
 
 .profile-row {
     display: flex;
+    justify-content: center;
 }
 
 .profile-row .dp {
@@ -121,80 +123,6 @@
         transform: rotate(-360deg);
     }
 }
-
-.container {
-    width: 50%;
-    display: flex;
-    justify-content: right;
-    align-items: right;
-}
-
-.tree {
-    position: relative;
-    width: 50px;
-    height: 50px;
-    transform-style: preserve-3d;
-    transform: rotateX(-20deg) rotateY(30deg);
-    animation: treeAnimate 5s linear infinite;
-}
-
-@keyframes treeAnimate {
-    0% {
-        transform: rotateX(-20deg) rotateY(360deg);
-    }
-
-    100% {
-        transform: rotateX(-20deg) rotateY(0deg);
-    }
-}
-
-.tree div {
-    position: absolute;
-    top: -50px;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transform-style: preserve-3d;
-    transform: translateY(calc(25px * var(--x))) translateZ(0px);
-}
-
-.tree div.branch span {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(90deg, #69c069, #77dd77);
-    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
-    border-bottom: 5px solid #00000019;
-    transform-origin: bottom;
-    transform: rotateY(calc(90deg * var(--i))) rotateX(30deg) translateZ(28.5px);
-}
-
-.tree div.stem span {
-    position: absolute;
-    top: 110px;
-    /* updated top value */
-    left: calc(50% - 7.5px);
-    width: 15px;
-    height: 50%;
-    background: linear-gradient(90deg, #bb4622, #df7214);
-    border-bottom: 5px solid #00000019;
-    transform-origin: bottom;
-    transform: rotateY(calc(90deg * var(--i))) translateZ(7.5px);
-}
-
-.shadow {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.4);
-    filter: blur(20px);
-    transform-style: preserve-3d;
-    transform: rotateX(90deg) translateZ(-65px);
-}
 </style>
 
 <body id="page-top">
@@ -204,6 +132,8 @@
         <div class="header-content-inner">
             <h3 class="text-center" style="color:green;">Pembayaran Virtual Account</h3><br>
             <hr class="hrcenter">
+            <p class="text-center">*Pembayaran dengan Virtual Account Dapat Melalui ATM atau Mobile Banking atau
+                Internet Banking</p>
             <div class="id-card-wrapper center">
                 <div class="id-card">
                     <div class="profile-row">
@@ -217,42 +147,9 @@
                             <p>Saat Ini Kami Tidak Melayani Pembayaran Virtual Account</p>
                         </div>
                     </div>
+
                 </div>
-                <div class="container">
-                    <div class="tree">
-                        <div class="branch" style="--x:0">
-                            <span style="--i:0;"></span>
-                            <span style="--i:1;"></span>
-                            <span style="--i:2;"></span>
-                            <span style="--i:3;"></span>
-                        </div>
-                        <div class="branch" style="--x:1">
-                            <span style="--i:0;"></span>
-                            <span style="--i:1;"></span>
-                            <span style="--i:2;"></span>
-                            <span style="--i:3;"></span>
-                        </div>
-                        <div class="branch" style="--x:2">
-                            <span style="--i:0;"></span>
-                            <span style="--i:1;"></span>
-                            <span style="--i:2;"></span>
-                            <span style="--i:3;"></span>
-                        </div>
-                        <div class="branch" style="--x:3">
-                            <span style="--i:0;"></span>
-                            <span style="--i:1;"></span>
-                            <span style="--i:2;"></span>
-                            <span style="--i:3;"></span>
-                        </div>
-                        <div class="stem">
-                            <span style="--i:0;"></span>
-                            <span style="--i:1;"></span>
-                            <span style="--i:2;"></span>
-                            <span style="--i:3;"></span>
-                        </div>
-                        <span class="shadow"></span>
-                    </div>
-                </div>
+
             </div>
 
         </div>
